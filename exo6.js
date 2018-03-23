69b833031022b031941db795b13974af6245922e
JonathanMarandeau <jonathan.marandeau@gmail.com>
2018-03-23 12:02:16 +0100

    modif

--- exo6.js
+++ exo6.js
@@ -1,4 +1,4 @@
-function calcul() {
+function resultat() {
   var firstnumber = parseFloat(document.getElementById("premier_nombre").value);
   var secondnumber = parseFloat (document.getElementById("deuxieme_nombre").value);
   var result = Math.trunc(firstnumber) * secondnumber;
