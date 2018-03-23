17ba5858b906f2c9c84a0208a7a2531980028d0c
JonathanMarandeau <jonathan.marandeau@gmail.com>
2018-03-23 11:20:17 +0100

    Exercice 6 avec deux fichiers

---
+++ exo6.js
@@ -0,0 +1,6 @@
+function calcul() {
+  var firstnumber = parseFloat(document.getElementById("premier_nombre").value);
+  var secondnumber = parseFloat (document.getElementById("deuxieme_nombre").value);
+  var result = Math.trunc(firstnumber) * secondnumber;
+    alert(result);
+}
